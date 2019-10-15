bea878adb79c7f5ad90d71536ff07609c459082d
alexa984 <alex.yovkova@gmail.com>
2019-10-15 21:36:48 +0300

    Print bigger arrays with formatter

--- arrays.rs
+++ arrays.rs
@@ -1,5 +1,22 @@
+use std::fmt;
+
+struct Array<T> {
+    data: [T; 1024]
+}
+
+impl<T: fmt::Debug> fmt::Debug for Array<T> {
+    fn fmt(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        self.data[..].fmt(formatter)
+    }
+}
+
 fn main(){
     // create array with 100 zeroes
-    let arr = [0; 100];
-    println!("{:?}", &arr[..]);
+    let arr = [0; 10];
+    println!("{:?}", arr);
+
+    //print bigger arrays this way instead of using &
+    let array = Array { data: [0; 1024] };
+    println!("{:?}", array);
+
 }
