52d1229398ba8d864d2bbeb20f89f8960ad6d24a
alexa984 <alex.yovkova@gmail.com>
2019-10-24 17:31:42 +0300

    Parsing string to num

--- strings.rs
+++ strings.rs
@@ -4,6 +4,9 @@ fn main(){
     let s1_len = calculate_length(&s1);
     println!("{}", s2);
     println!("The length of s1 is: {}", s1_len);
+
+    let num: u32 = "42".parse().expect("Not a number!"); // parse string to number
+    println!("{}", num);
 }
 
 fn calculate_length(s: &String) -> usize {
