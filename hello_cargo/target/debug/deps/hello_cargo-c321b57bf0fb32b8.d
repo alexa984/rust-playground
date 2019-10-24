71a5b069d511760c552a109f0b00d010adbf02da
alexa984 <alex.yovkova@gmail.com>
2019-10-24 15:53:16 +0300

    Introduce cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-c321b57bf0fb32b8.d
@@ -0,0 +1,5 @@
+/home/alexandra/Documents/Rust-playground/hello_cargo/target/debug/deps/hello_cargo-c321b57bf0fb32b8: src/main.rs
+
+/home/alexandra/Documents/Rust-playground/hello_cargo/target/debug/deps/hello_cargo-c321b57bf0fb32b8.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/alexandra/Documents/Rust-playground/hello_cargo/target/debug/hello_cargo: /home/alexandra/Documents/Rust-playground/hello_cargo/src/main.rs
